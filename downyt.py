d3a77cce53c7da369778730a56a788ad2a7d4064
Phanupong48 <pongs9988@gmail.com>
2025-07-14 21:56:33 +0700

    start and comment

---
+++ downyt.py
@@ -0,0 +1,11 @@
+import yt_dlp   #นำเข้าไลบรารีชื่อ yt_dlp เพื่อใช้ดาวน์โหลดวิดีโอจาก YouTube
+def download_youtube_video(url, save_path="."): #ตั้งชื่อฟังก์ชันชื่อ download_youtube_video และมีการรับพารามิเตอร์ 2 ตัว
+    ydl_opts = {    #กำหนดตัวเลือกให้กับ yt_dlp
+        'outtmpl': f'{save_path}/%(title)s.%(ext)s',
+        'format': 'best' #ดาวโหลดคุณภาพดี
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl: #สร้าง object YoutubeDL โดยใช้ options ที่เลือก ใช้ ydl.download([url]) เพื่อดาวน์โหลดวิดีโอจากลิงก์ที่ใส่มา  
+        ydl.download([url]) #ดาวโหลดจาก url
+video_url = input("Enter the YouTube video URL: ") #รับ Url จากผู้ใช้ผ่านทางและแสดงข้อความ "Enter the YouTube video URL: "
+
+download_youtube_video(video_url, save_path=".") #เรียกใช้ฟังก์ชัน download_youtube_video
